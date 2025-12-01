3510c0300ff7d91e6d224c2c3756515d7c51a1cc
Piotr <pipi6620@gmail.com>
2025-12-01 12:53:00 +0100

    NotFound component added

--- frontend/src/components/NotFound/not-found.tsx
+++ frontend/src/components/NotFound/not-found.tsx
@@ -1,3 +1,39 @@
+import {
+  Empty,
+  EmptyDescription,
+  EmptyMedia,
+  EmptyTitle,
+  EmptyHeader,
+} from "@/components/ui/empty";
+import { Button } from "@/components/ui/button";
+import { BookDashed, ArrowUpRight } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
-  return <div>404 Not Found. The page you are looking for does not exist.</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full">
+      <Empty className="max-w-md border border-dashed from-muted/50 to-background bg-linear-to-b from-30%">
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <BookDashed />
+          </EmptyMedia>
+          <EmptyTitle>404 Page Not Found</EmptyTitle>
+          <EmptyDescription>
+            The page you are looking for does not exist. Please check the URL or
+            go back to the homepage.
+          </EmptyDescription>
+        </EmptyHeader>
+        <Button
+          variant="link"
+          asChild
+          className="text-muted-foreground"
+          size="sm"
+        >
+          <Link to="/" rel="noreferrer noopener">
+            Return to Dashboard <ArrowUpRight />
+          </Link>
+        </Button>
+      </Empty>
+    </div>
+  );
 };
