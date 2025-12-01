34045d2cb79e25d98140cc38aec4fc6f9de0e3e4
Piotr <pipi6620@gmail.com>
2025-12-01 13:08:53 +0100

    Created example Error component

--- frontend/src/components/About/about.tsx
+++ frontend/src/components/About/about.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import { Error as ErrorComponent } from "@/components/Error/error";
+
 export const About = () => {
+  const [error, setError] = useState<Error | null>(Error);
+
+  if (error) {
+    return <ErrorComponent />;
+  }
+
   return (
     <div>
       <h1>About</h1>
--- frontend/src/components/Error/error.tsx
+++ frontend/src/components/Error/error.tsx
@@ -1,3 +1,55 @@
+import {
+  Empty,
+  EmptyDescription,
+  EmptyMedia,
+  EmptyTitle,
+  EmptyHeader,
+  EmptyContent,
+} from "@/components/ui/empty";
+import { TriangleAlert } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Spinner } from "../ui/spinner";
+
 export const Error = () => {
-  return <div>An error has occurred. Please try again later.</div>;
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const handleRefresh = () => {
+    setIsRefreshing(true);
+    setInterval(() => window.location.reload(), 1000);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full">
+      <Empty className="max-w-md border border-dashed from-muted/50 to-background bg-linear-to-b from-30%">
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <TriangleAlert />
+          </EmptyMedia>
+          <EmptyTitle>Error</EmptyTitle>
+          <EmptyDescription>
+            An internal error has occurred. Please try again later.
+          </EmptyDescription>
+        </EmptyHeader>
+
+        <EmptyContent>
+          <div className="flex gap-2">
+            <Button
+              variant="outline"
+              onClick={handleRefresh}
+              disabled={isRefreshing}
+            >
+              {isRefreshing ? (
+                <>
+                  <Spinner /> Refreshing...
+                </>
+              ) : (
+                "Refresh"
+              )}
+            </Button>
+          </div>
+        </EmptyContent>
+      </Empty>
+    </div>
+  );
 };
